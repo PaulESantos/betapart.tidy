eb36881d96d5e0c7d60cf033f2e5d3c877293087
Paul Efren Santos Andrade <paulefrens@gmail.com>
2018-10-15 19:11:35 -0500

    tidy rarefraction functions

--- R/plot_rarecurve.R
+++ R/plot_rarecurve.R
@@ -1,66 +1,107 @@
 #' plot_rarecurve
+#' @description
 #'
-#' @param comm Community data, a matrix.
+#'   Draws a rarefaction curve for each row of the input data.
+#' @param comm Community data.
+#' @param facet.var Index for changing the output plot:
+#'  "none" the default output.
+#'  "sites" the output is a multi-panel plot.
 #'
-#' @return A plot class **ggplot**.
+#' @return
 #' @export
 #'
 #' @examples
 #' require(vegan)
-#' data("dune")
-#'
+#' data(dune)
 #' plot_rarecurve(dune)
-#' Manipulate the output with tidyverse.
-#' plot_rarecurve(dune) +
-#'   facet_wrap(.~sites) +
-#'   theme(legend.position = "none")
+#' plot_rarecurve(dune, facet.var = "sites")
+#'
+#'
+plot_rarecurve <-  function (comm, facet.var = "none")
+  {
+    SPLIT <- c("none","sites")
+    if (is.na(pmatch(facet.var, SPLIT)) | pmatch(facet.var,
+                                                 SPLIT) == -1)
+      stop("invalid facet variable")
+    facet.var <- match.arg(facet.var, SPLIT)
 
-plot_rarecurve <- function(comm){
-
-  rare <- function (comm, step = 1, sample) {
-    x <- as.matrix(comm)
-    if (!identical(all.equal(x, round(x)), TRUE))
-      stop("function accepts only integers (counts)")
-    tot <- rowSums(x)
-    S <- vegan::specnumber(x)
-    if (any(S <= 0)) {
-      message("empty rows removed")
-      x <- x[S > 0, , drop = FALSE]
-      tot <- tot[S > 0]
-      S <- S[S > 0]
+    rare <- function(comm, step = 1, sample) {
+      x <- as.matrix(comm)
+      if (!identical(all.equal(x, round(x)), TRUE))
+        stop("function accepts only integers (counts)")
+      tot <- rowSums(x)
+      S <- vegan::specnumber(x)
+      if (any(S <= 0)) {
+        message("empty rows removed")
+        x <- x[S > 0, , drop = FALSE]
+        tot <- tot[S > 0]
+        S <- S[S > 0]
+      }
+      nr <- nrow(x)
+      out <- lapply(seq_len(nr), function(i) {
+        n <- seq(1, tot[i], by = step)
+        if (n[length(n)] != tot[i])
+          n <- c(n, tot[i])
+        drop(vegan::rarefy(x[i, ], n))
+      })
+      return(out)
     }
-    nr <- nrow(x)
-    out <- lapply(seq_len(nr), function(i) {
-      n <- seq(1, tot[i], by = step)
-      if (n[length(n)] != tot[i])
-        n <- c(n, tot[i])
-      drop(vegan::rarefy(x[i, ], n))
-    })
-    return(out)
+    meta <- dplyr::data_frame(name = rownames(comm),
+                              sites = seq(1,length(name)))
+
+    dat <- rare(comm)
+
+    dat <- dat %>%
+      reshape2::melt() %>%
+      dplyr::as_data_frame() %>%
+      dplyr::rename(species = "value", sites = "L1")
+
+    dat1 <- meta %>%
+      dplyr::full_join(dat, by = "sites")%>%
+      dplyr::group_by(name) %>%
+      dplyr::mutate(size = seq(1,length(sites), 1))
+
+    output <- dat1  %>%
+      ggplot2::ggplot(aes(size, species)) +
+      ggplot2::geom_line(aes(group = name), size = 1, color = "grey") +
+      ggplot2::theme_bw() +
+      ggplot2::geom_text(data = dat1 %>% filter(size == last(size)),
+                         aes(
+                           label = name,
+                           x = size + 0.5,
+                           y = species,
+                           color = sites
+                         ), size = 5)+
+      ggplot2::scale_y_continuous(breaks = seq(1, 1000, 10)) +
+      ggplot2::theme(
+        text = element_text(face = "bold", size = 10),
+        axis.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 12)) +
+      ggplot2::labs(x = "Sample Size", y = "Species") +
+      ggplot2::guides(color = FALSE)
+
+
+    if (facet.var == "none") {
+      return(output)
+    }
+    else (facet.var == "sites")
+    {
+      output <- dat1 %>%
+        ggplot2::ggplot(aes(size, species)) +
+        ggplot2::geom_line(aes(group = name), size = .8, color = "green") +
+        ggplot2::theme_bw() +
+        ggplot2::scale_y_continuous(breaks = seq(1, 1000, 10)) +
+        ggplot2::theme(axis.title = element_text(face = "bold", size = 12),
+                       strip.text.x = element_text(size = 12, face = "bold"),
+                       strip.background = element_rect(fill = "white"),
+                       text = element_text(face = "bold", size = 10),
+                       axis.text = element_text(face = "bold", size = 10),
+                       panel.grid = element_blank()) +
+        ggplot2::facet_wrap(.~name)+
+        ggplot2::labs(x = "Sample Size", y = "Species", color = "Sites")+
+        ggplot2::theme(legend.position = "none")
+      return(output)
+    }
+    class(output) <- c("gg", "ggplot")
+    return(output)
   }
-
-  dat <- rare(comm)
-
-  output <- dat %>%
-    reshape2::melt() %>%
-    dplyr::as_data_frame() %>%
-    dplyr::rename( "species" = "value","sites" = "L1") %>%
-    dplyr::mutate(sites = as.character(sites)) %>%
-    dplyr::group_by(sites) %>%
-    dplyr::mutate(size = seq(1, length(sites), 1)) %>%
-    ggplot2::ggplot(aes(size, species, color = sites))+
-    ggplot2::geom_line(aes(group = sites), size = 1)+
-    ggplot2::theme_bw()+
-    ggplot2::scale_y_continuous(breaks = seq(1, 100, 1))+
-    ggplot2::theme(legend.position = "bottom",
-                   strip.text.x = element_text(size = 18, face = "bold"),
-                   strip.background = element_rect(fill = "white"),
-                   text = element_text(face = "bold", size = 10),
-                   legend.text = element_text(face = "bold", size = 12))+
-    ggplot2::labs(x = "Sample Size",
-                  y = "Species",
-                  color = "Sites") +
-    ggplot2::guides(color = guide_legend(ncol = 10))
-  class(output) <- c("gg", "ggplot")
-  return(output)
-}
